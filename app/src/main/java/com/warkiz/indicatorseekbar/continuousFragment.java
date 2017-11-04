41a373ab3b1fca6be4bb96a3dadb59f75b32a019
zhuangguangquan <846674284@qq.com>
2017-11-04 13:54:51 +0800

    show indicator default

---
+++ app/src/main/java/com/warkiz/indicatorseekbar/continuousFragment.java
@@ -0,0 +1,22 @@
+package com.warkiz.indicatorseekbar;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 创建者: Administrator
+ * 创建时间: 2017/11/4 11:28
+ * 描述： TODO
+ */
+
+public class continuousFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.custom, container, false);
+    }
+
+}

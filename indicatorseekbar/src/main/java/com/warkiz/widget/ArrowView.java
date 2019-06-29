ecca48b79d49ab479fbc306245a0e64c38d25084
zhuangguangquan <846674284@qq.com>
2019-06-29 22:29:44 +0800

    update readme

--- indicatorseekbar/src/main/java/com/warkiz/widget/ArrowView.java
+++ indicatorseekbar/src/main/java/com/warkiz/widget/ArrowView.java
@@ -13,9 +13,11 @@ import android.view.View;
  * https://github.com/warkiz/IndicatorSeekBar
  * <p>
  * Donation/打赏:
- * If this library is helpful to you ,you can give me a donation by @see <a href="https://www.paypal.me/BuyMeACupOfTeaThx">ZhuanGuangQuan's Paypal</a>,
- * or @see <a href="https://github.com/warkiz/IndicatorSeekBar/blob/master/app/src/main/res/mipmap-xxhdpi/wechat_pay.png?raw=true">微信支付</a>,
- * or @see <a href="https://github.com/warkiz/IndicatorSeekBar/blob/master/app/src/main/res/mipmap-xxhdpi/alipay.png?raw=true">支付宝</a>
+ * If this library is helpful to you ,you can give me a donation by:
+ *
+ * @see <a href="https://www.paypal.me/BuyMeACupOfTeaThx">ZhuanGuangQuan's Paypal</a>, or
+ * @see <a href="https://github.com/warkiz/IndicatorSeekBar/blob/master/app/src/main/res/mipmap-xxhdpi/wechat_pay.png?raw=true">微信支付</a>, or
+ * @see <a href="https://github.com/warkiz/IndicatorSeekBar/blob/master/app/src/main/res/mipmap-xxhdpi/alipay.png?raw=true">支付宝</a>
  * <p>
  */
 
